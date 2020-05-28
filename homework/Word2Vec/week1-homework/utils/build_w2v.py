61657be3e6178d8d888c1aa093f80d36f8bfc1bd
hongfu <tthf@sina.com>
2020-05-28 10:15:17 +0800

    modify

--- homework/Word2Vec/week1-homework/utils/build_w2v.py
+++ homework/Word2Vec/week1-homework/utils/build_w2v.py
@@ -51,10 +51,11 @@ def build(train_x_seg_path, test_y_seg_path, test_seg_path, out_path=None, sente
     w2v.wv.save_word2vec_format(w2v_bin_path, binary=True)
     print("save %s ok." % w2v_bin_path)
     # test
-    sim = w2v.wv.similarity('技师', '车主')
-    print('技师 vs 车主 similarity score:', sim)
+    # sim = w2v.wv.similarity('技师', '车主')
+    # print('技师 vs 车主 similarity score:', sim)
     # load model
     model = KeyedVectors.load_word2vec_format(w2v_bin_path, binary=True)
+    # model = KeyedVectors.load(w2v_bin_path)
     word_dict = {}
     for word in model.vocab:
         word_dict[word] = model[word]
